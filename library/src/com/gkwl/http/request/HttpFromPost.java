1cff7db7dc4061577d6bc34a6ff24a0d744d1cb9
gkwl <geekkwl@gmail.com>
2014-10-10 17:55:53 +0800

    convenient class for web form submit.

---
+++ library/src/com/gkwl/http/request/HttpFromPost.java
@@ -0,0 +1,13 @@
+package com.gkwl.http.request;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class HttpFromPost extends HttpPost {
+	@Override
+	protected void appendHeaders(OutputStream os) throws NullPointerException, IOException {
+		super.appendHeaders(os);
+		if (!reqHeaders.containsKey("Content-Length"))
+			os.write(("Content-Type: application/x-www-form-urlencoded" + CL).getBytes());
+	}
+}
